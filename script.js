26fb00371b4057a9b0ff3f716e52d150df22f5c8
Zaliya Validova <z.validova@yandex.ru>
2022-01-28 20:21:04 +0500

    Добавление 2 задания

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-const title = "Заголовок";
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+const title = "Название проекта";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 300;
+let rollback = 99;
+let fullPrice = 80000;
+let adaptive = true;
 
-alert('Привет!!!');
-console.log('Как дела?');
+console.log(title);
+console.log(fullPrice);
+console.log(adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта " + fullPrice +" гривен");
+
+screens = screens.toLowerCase().split(" ");
+console.log(screens);
+
+console.log(fullPrice * (rollback/100));
