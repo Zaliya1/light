0f84b8df872b43134615e7ca929e7b10c7b843a1
Zaliya Validova <z.validova@yandex.ru>
2022-02-08 19:16:25 +0500

    Добавление 9 дз

---
+++ js/script.js
@@ -0,0 +1,39 @@
+'use strict'
+//1 задание
+const title = document.getElementsByTagName('h1')[0];
+console.log(title);
+//2 задание
+const buttons = document.getElementsByClassName('handler_btn');
+const buttonStart = buttons[0];
+const buttonReset = buttons[1];
+console.log(buttonStart);
+console.log(buttonReset);
+//3 задание
+const buttonScreen = document.querySelector('.screen-btn');
+console.log(buttonScreen);
+//4 задание
+const items = document.querySelectorAll('.other-items,.percent');
+const percentItems = document.querySelectorAll('.other-items.percent');
+const numberItems = document.querySelectorAll('.other-items.number');
+// console.log(percentItems);
+// console.log(numberItems);
+percentItems.forEach(function (item) {
+    console.log(item);
+});
+numberItems.forEach(function (item) {
+    console.log(item);
+});
+//5 и 6 задания
+const inputRange = document.querySelector('.rollback > .main-controls__range > input[type="range"]');
+const span = document.querySelector('.rollback > .main-controls__range > .range-value');
+console.log(inputRange);
+console.log(span);
+//7 задание
+const inputs = document.getElementsByClassName('total-input');
+console.log(inputs);
+for (let i = 0; i< inputs.length; i++) {
+    console.log(inputs[i]);
+}
+//8 задание
+let screens = document.querySelectorAll('.screen');
+console.log(screens);
